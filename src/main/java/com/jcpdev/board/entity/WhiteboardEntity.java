bb85b69c7a347b9c6843a2c843c32af921d47d52
Beomki <Beomki@DESKTOP-8686GCD>
2021-11-15 17:46:33 +0900

    add ManyToOne clientEntity

--- src/main/java/com/jcpdev/board/entity/WhiteboardEntity.java
+++ src/main/java/com/jcpdev/board/entity/WhiteboardEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -49,4 +50,6 @@ public class WhiteboardEntity {
 	@Column(nullable = false)
 	private Integer whiteboard_like;
 
+	@ManyToOne(targetEntity = ClientEntity.class)
+	private ClientEntity clientEntity;
 }
