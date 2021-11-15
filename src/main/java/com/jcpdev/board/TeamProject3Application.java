7cae10440dac662a61d150eb79185a776af74e5f
Kimbeomjin <advensod@daum.net>
2021-11-15 11:12:56 +0900

    11/15 11:12

---
+++ src/main/java/com/jcpdev/board/TeamProject3Application.java
@@ -0,0 +1,13 @@
+package com.jcpdev.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProject3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcpdev/board/TeamProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcpdev.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
