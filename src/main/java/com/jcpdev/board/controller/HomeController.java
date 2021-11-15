21fb0cc3fe40824d6577934c715d80ec47cc13ef
SBKi <beomki7429@gmail.com>
2021-11-15 15:22:58 +0900

    ㅋㅁ

---
+++ src/main/java/com/jcpdev/board/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jcpdev.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/instagram")
+	public void instagram() {
+		
+	}
+}
