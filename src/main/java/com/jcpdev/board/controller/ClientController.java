5d45d5e53d16b86e3cbc1249deafc3d027a7b393
Kimbeomjin <advensod@daum.net>
2021-11-19 11:24:47 +0900

    11/19 11:24

--- src/main/java/com/jcpdev/board/controller/ClientController.java
+++ src/main/java/com/jcpdev/board/controller/ClientController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -136,8 +135,11 @@ public class ClientController {
 		ClientEntity entity = service.toEntity(client);
 		repository.save(entity);
 			return "redirect:login";
-			
 	}
+	
+	
+	
+	
 	
 	
 
--- src/main/java/com/jcpdev/board/service/ClientService.java
+++ src/main/java/com/jcpdev/board/service/ClientService.java
@@ -5,13 +5,12 @@ import com.jcpdev.board.model.Client;
 
 public interface ClientService {
 	default ClientEntity toEntity(Client dto) {
-
 		return ClientEntity.builder().client_id(dto.getClient_id()).client_password(dto.getClient_password())
 				.client_email(dto.getClient_email()).client_name(dto.getClient_name())
 				.client_address(dto.getClient_address()).client_gender(dto.getClient_gender())
 				.client_birth(dto.getClient_birth()).client_birth(dto.getClient_birth())
 				.client_tall(dto.getClient_tall()).client_img(dto.getClient_img())
-				.client_instruction(dto.getClient_instruction()).client_info(dto.getClient_info()).build();
+				.client_instruction(dto.getClient_instruction()).client_info(dto.getClient_info()).client_status(dto.getClient_status()).build();
 	}
 
 	default Client toDto(ClientEntity entity) {
@@ -20,7 +19,7 @@ public interface ClientService {
 				.client_address(entity.getClient_address()).client_gender(entity.getClient_gender())
 				.client_birth(entity.getClient_birth()).client_tall(entity.getClient_tall())
 				.client_img(entity.getClient_img()).client_instruction(entity.getClient_instruction())
-				.client_info(entity.getClient_info()).build();
+				.client_info(entity.getClient_info()).client_status(entity.getClient_status()).build();
 	}
 
 }
